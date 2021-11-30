486c6bf737bedd7f98ade3a70d90644b62cd19de
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-30 01:23:27 -0100

    User and userTypes models

--- src/models/UserTypes.ts
+++ src/models/UserTypes.ts
@@ -1,26 +1,31 @@
-import { Model, DataTypes } from "sequelize";
-import { sequelize } from '../config/pg';
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from '../config/pg'
+import { user } from './User'
 
 export interface UserTypesInstance extends Model {
-    idUsertype: number,
+  idUsertype: number
 
-    type: string
+  type: string
 }
 
-export const userTypes = sequelize.define<UserTypesInstance>('userTypes', {
+export const userTypes = sequelize.define<UserTypesInstance>(
+  'userTypes',
+  {
     idUsertype: {
-        primaryKey: true,
-        unique: true,
-        autoIncrement: true,
-        type: DataTypes.INTEGER,
+      primaryKey: true,
+      unique: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
     },
 
     type: {
       allowNull: false,
-      type: DataTypes.STRING(50)
+      type: DataTypes.STRING(50),
     },
-}, {
+  },
+  {
     tableName: 'userTypes',
     freezeTableName: true,
-    timestamps: false, 
-});
+    timestamps: false,
+  }
+)
