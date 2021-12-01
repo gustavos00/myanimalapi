f9567b03c70a6923c97cf15ef7751cb35d242bd5
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-01 03:03:27 -0100

    Adding a delete endpoint on routes

--- src/routes/animal.ts
+++ src/routes/animal.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import * as animalController from '../controllers/animalController';
 
 const router = Router();
-router.post('/create', animalController.create);
-//router.post('/update')
+router.post('/create', animalController.createAnimal);
+router.delete('/delete/:id', animalController.deleteAnimal);
+router.post('/update');
 //router.post('/read')
 
 export default router;
