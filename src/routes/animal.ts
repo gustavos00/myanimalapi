baed5ca2acf57294fb25791671b59a46c1135552
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-01 02:43:35 -0100

    Creating the route and importing him to routes index

--- src/routes/animal.ts
+++ src/routes/animal.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
+import * as animalController from '../controllers/animalController';
 
 const router = Router();
-router.get('/create')
-//router.get('/update')
-//router.get('/read')
+router.post('/create', animalController.create);
+//router.post('/update')
+//router.post('/read')
 
 export default router;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import generalRoutes from './general'
-import userRoutes from './user'
+import generalRoutes from './general';
+import userRoutes from './user';
+import animalRoutes from './animal';
 
 const router = Router();
-router.use('/general', generalRoutes)
-router.use('/user', userRoutes)
+router.use('/general', generalRoutes);
+router.use('/user', userRoutes);
+router.use('/animal', animalRoutes);
 
 export default router;
