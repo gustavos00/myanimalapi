f4cf201fd4c474a379c4040fe0daaaaa3de0f348
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-04 02:44:32 -0100

    Creating a updateSchema

--- src/schemas/animalSchema.ts
+++ src/schemas/animalSchema.ts
@@ -12,5 +12,17 @@ export const createAnimalSchema = Joi.object({
 });
 
 export const deleteAnimalSchema = Joi.object({
-  id: Joi.number().required(),
+  id: Joi.string().required(),
+});
+
+export const updateAnimalSchema = Joi.object({
+  id: Joi.string().required(),
+  name: Joi.string().required().max(250),
+  age: Joi.string().max(5),
+  breed: Joi.string().required().max(250),
+  birthday: Joi.string().max(2),
+  birthdayMonth: Joi.string().max(2),
+  trackNumber: Joi.string().max(50),
+
+  token: Joi.string().required(),
 });
