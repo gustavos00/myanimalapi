e2fff91597595bf5d2bde1c022004a4981712c64
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-04 21:33:31 -0100

    Heroku

--- src/controllers/GeneralController.ts
+++ src/controllers/GeneralController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const ping = (req: Request, res: Response) => {
-    res.status(200).send({ message: 'pong' });
+  res.status(200).send({ message: true });
 };
 
 export default ping;
