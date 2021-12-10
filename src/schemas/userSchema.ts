d7b4d653358de42d87869881c50b24f52d084e42
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-10 22:00:34 -0100

    Creating createAddress schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,7 +1,13 @@
 const Joi = require("joi");
 
 export const createUserSchema = Joi.object({
-  givenName: Joi.string().required().max(60),
-  familyName: Joi.string().required().max(60),
-  email: Joi.string().required().email(),
+  givenName: Joi.string().required().min(1).max(60),
+  familyName: Joi.string().required().min(1).max(60),
+  email: Joi.string().required().min(1).email(),
 });
+
+export const createAddressSchema = Joi.object({
+  streetName: Joi.string().required().min(1).max(100),
+  postalCode: Joi.string().required().min(1).max(12),
+  doorNumber: Joi.string().required().min(1).max(10),
+})
