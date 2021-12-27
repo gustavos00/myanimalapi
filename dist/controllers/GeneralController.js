e7a82bd9570711dc3567354d81f68d3713364666
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-27 00:32:52 +0000

    Remove current dist

--- dist/controllers/GeneralController.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ping = (req, res) => {
-    res.status(200).send({ message: 'pong' });
-};
-exports.default = ping;
