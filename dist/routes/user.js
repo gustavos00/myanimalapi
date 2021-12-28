17603011afe8c1ceca6e52a20e4c53f93bc964d8
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-28 22:49:50 +0000

    Update dist

---
+++ dist/routes/user.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const multer = require('multer');
+const multerConfig = require('../config/multer');
+const findOrCreateUser_1 = __importDefault(require("../controllers/user/findOrCreateUser"));
+const verifyAccessToken_1 = __importDefault(require("../controllers/user/verifyAccessToken"));
+const createAddress_1 = __importDefault(require("./../controllers/user/createAddress"));
+const generateAndVerifyQRToken_1 = require("./../controllers/user/generateAndVerifyQRToken");
+const router = (0, express_1.Router)();
+router.post('/create', multer(multerConfig).single('userPhoto'), findOrCreateUser_1.default);
+router.post('/createAddress', createAddress_1.default);
+router.get('/friend/token/', generateAndVerifyQRToken_1.generateToken);
+router.get('/friend/verifyToken/', generateAndVerifyQRToken_1.verifyToken);
+router.post('/access/verify/', verifyAccessToken_1.default);
+exports.default = router;
