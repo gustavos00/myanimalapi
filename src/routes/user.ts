d90706d926986ecea5b1fbffe6e3adb5589947b6
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-28 06:35:04 +0000

    Adding the verify access controller

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,14 @@
-
 import { Router } from 'express';
 const multer = require('multer');
 const multerConfig = require('../config/multer');
 
 import findOrCreate from '../controllers/user/findOrCreateUser';
+import verifyAccessToken from '../controllers/user/verifyAccessToken';
 import createAddress from './../controllers/user/createAddress';
-import { generateToken, verifyToken}  from './../controllers/user/generateAndVerifyQRToken';
-
+import {
+  generateToken,
+  verifyToken,
+} from './../controllers/user/generateAndVerifyQRToken';
 
 const router = Router();
 router.post('/create', multer(multerConfig).single('userPhoto'), findOrCreate);
@@ -14,5 +16,7 @@ router.post('/createAddress', createAddress);
 
 router.get('/friend/token/', generateToken);
 router.get('/friend/verifyToken/', verifyToken);
+
+router.post('/access/verify/', verifyAccessToken);
 
 export default router;
