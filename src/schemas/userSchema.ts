97d8c7cb92fd3e3cff0bb014cb6d494cf6efd69e
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-28 06:35:22 +0000

    verify access token schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,9 +1,10 @@
-const Joi = require("joi");
+const Joi = require('joi');
 
 export const findOrCreateUserSchema = Joi.object({
   givenName: Joi.string().required().min(1).max(60),
   familyName: Joi.string().required().min(1).max(60),
   email: Joi.string().required().min(1).email(),
+  salt: Joi.string().required().min(1),
 });
 
 export const createAddressSchema = Joi.object({
@@ -11,18 +12,23 @@ export const createAddressSchema = Joi.object({
   postalCode: Joi.string().required().min(1).max(12),
   doorNumber: Joi.string().required().min(1).max(10),
   email: Joi.string().email().required(),
-})
+});
 
 export const statusSchema = Joi.object({
   status: Joi.string().required().min(1).max(100),
-})
+});
 
 export const generateTokenSchema = Joi.object({
   email: Joi.string().required().email(),
-  id: Joi.string().required().min(1)
-})
+  id: Joi.string().required().min(1),
+});
 
 export const verifyTokenSchema = Joi.object({
   token: Joi.string().required().min(1),
   fromWho: Joi.string().required().min(1),
-})
+});
+
+export const verifyAccessToken = Joi.object({
+  token: Joi.string().required().min(1),
+  salt: Joi.string().required().min(1),
+});
