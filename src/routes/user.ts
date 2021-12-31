91ba2db332e4e2340872ce9780098e09166154a6
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-31 04:50:51 +0000

    Adding get all friends controller in user routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,9 +3,11 @@ const multer = require('multer');
 const multerConfig = require('../config/multer');
 
 import findOrCreate from '../controllers/user/findOrCreateUser';
+import getAllFriendsRequest from '../controllers/user/getAllFriendsRequests';
 import UpdateUser from '../controllers/user/updateUser';
 import verifyAccessToken from '../controllers/user/verifyAccessToken';
 import createAddress from './../controllers/user/createAddress';
+
 import {
   generateToken,
   verifyToken,
@@ -17,6 +19,7 @@ router.post('/update', multer(multerConfig).single('userPhoto'), UpdateUser);
 router.post('/createAddress', createAddress);
 
 router.get('/friend/token/', generateToken);
+router.get('/friend/get/', getAllFriendsRequest);
 router.get('/friend/verifyToken/', verifyToken);
 
 router.post('/access/verify/', verifyAccessToken);
