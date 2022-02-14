c2f947c0102a70e4040e0b01d143e1a5a568842b
Gustavo Santos <z.gsantos20@gmail.com>
2022-02-14 21:43:34 -0100

    Creating a get all veterinarians controller and create a veterinarians routes

---
+++ src/controllers/veterinarians/getAllVeterinarians.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import users from '../../models/User';
+
+const getAllVeterinarians = async (req: Request, res: Response) => {
+    let response;
+  try {
+      response = users.findAll({where: {isVeterinarian: true}});
+  } catch (e: any) {
+    console.log(
+      'Error getting all veterinarians on get all veterinarians controller'
+    );
+    res.status(500).send({ message: 'Something went wrong' });
+    throw new Error(e);
+  }
+
+  console.log(response)
+  res.status(200).send(response)
+};
+
+export default getAllVeterinarians
