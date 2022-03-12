e993b4658a9c0379486a6045d88274447fc4f294
Gustavo Santos <z.gsantos20@gmail.com>
2022-03-12 01:04:19 -0100

    Code formatting

--- src/utils/notifications.ts
+++ src/utils/notifications.ts
@@ -1,30 +1,32 @@
 import { Expo } from 'expo-server-sdk';
-const expo = new Expo()
+const expo = new Expo();
 
 interface sendNotificationsParams {
   expoToken?: string;
-  title: string
+  title: string;
   message: string;
-  data: object
+  data: object;
 }
 
-export const sendNotifications = async({
+export const sendNotifications = async ({
   expoToken,
   title,
   message,
   data,
 }: sendNotificationsParams) => {
-    if(!Expo.isExpoPushToken(expoToken)) {
-        return console.log('Its not a expo token')
-    }
+  if (!Expo.isExpoPushToken(expoToken)) {
+    return console.log('Its not a expo token');
+  }
 
-    const notificationMessage = [{
-        to: expoToken,
-        title,
-        body: message,
-        data,
-    }]
+  const notificationMessage = [
+    {
+      to: expoToken,
+      title,
+      body: message,
+      data,
+    },
+  ];
 
-    const receipt = await expo.sendPushNotificationsAsync(notificationMessage)
-    return receipt
+  const receipt = await expo.sendPushNotificationsAsync(notificationMessage);
+  return receipt;
 };
