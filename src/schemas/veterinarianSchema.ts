c4f55e84360e16e5fe9cf9f4a24d710130bf3994
Gustavo Santos <z.gsantos20@gmail.com>
2022-04-13 02:45:05 +0000

    Adding veterinarian schemas

--- src/schemas/veterinarianSchema.ts
+++ src/schemas/veterinarianSchema.ts
@@ -8,3 +8,22 @@ export const acceptVeterinarian = Joi.object({
 export const removeVeterinarian = Joi.object({
     animalId: Joi.string().required().min(1),
 })
+
+export const getVeterinarianAnimalsSchema = Joi.object({
+    id: Joi.string().required().min(1),
+})
+
+export const getAllEvents = Joi.object({
+    id: Joi.string().required().min(1),
+})
+
+export const updateEvent = Joi.object({
+    idEvents: Joi.number(),
+    report: Joi.string(),
+    eventsStatus: Joi.string(),
+    eventsTypes: Joi.string(),
+})
+
+export const getAllRequestedUsers = Joi.object({
+    id: Joi.string().required().min(1),
+})
