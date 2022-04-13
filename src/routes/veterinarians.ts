57f3123e1828e88a91d85eeb6059aba53add7e2e
Gustavo Santos <z.gsantos20@gmail.com>
2022-04-13 02:44:56 +0000

    Adding veterinarian routes

--- src/routes/veterinarians.ts
+++ src/routes/veterinarians.ts
@@ -1,14 +1,21 @@
 import { Router } from 'express';
 
-const multer = require('multer');
-const multerConfig = require('../config/multer');
 import removeVeterinarian from './../controllers/veterinarians/removeVeterinarian';
 import acceptVeterinarian from '../controllers/veterinarians/acceptVeterinarian';
-import getAllVeterinarians from '../controllers/veterinarians/getAllVeterinarians'
+import getAllVeterinarians from '../controllers/veterinarians/getAllVeterinarians';
+import getVeterinarianAnimals from '../controllers/veterinarians/getVeterinarianAnimals';
+import getAllEvents from '../controllers/veterinarians/getAllEvents';
+import updateEvent from '../controllers/veterinarians/updateEvents';
+import getAllEventsProps from '../controllers/veterinarians/getAllEventsProps';
 
 const router = Router();
-router.get('/get', getAllVeterinarians)
-router.post('/accept', acceptVeterinarian)
-router.post('/remove', removeVeterinarian)
+router.get('/get', getAllVeterinarians);
+router.get('/getAnimals', getVeterinarianAnimals);
+router.get('/getEvents', getAllEvents);
+router.get('/getEventsProps', getAllEventsProps);
+
+router.post('/updateEvent', updateEvent);
+router.post('/accept', acceptVeterinarian);
+router.post('/remove', removeVeterinarian);
 
 export default router;
